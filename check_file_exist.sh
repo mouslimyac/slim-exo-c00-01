5642bbcead2e28aeff14a658643ebd57402ba703
mouslimyac <mouslimyac@gmail.com>
2025-05-15 18:06:40 +0100

    modifications

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Ce script vérifie si un fichier donné existe ou non
-
-echo "Entrer le nom du fichier:"
+# Vérifie si un fichier existe
+#read -p "Entrez le nom du fichier : " filename
+ echo "Entrez le nom du fichier :"
 read filename
-
-if [ -f "$filename" ]; then
+if [ -f $filename ]; then
     echo "Le fichier '$filename' existe."
-else
+    else
+   
     echo "Le fichier '$filename' n'existe pas."
-fi
+fi 
